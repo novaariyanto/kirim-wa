af2bb446f21dd87b83335c7a2ea25a471bef357f
novaariyanto <bajangsampoerna@gmail.com>
2021-10-23 10:25:59 +0700

    master

---
+++ sendMessageText.php
@@ -0,0 +1,24 @@
+<?php 
+
+$curl = curl_init();
+        $data = [
+            "message" => "test bro",
+            "jid" => "6285156345912",
+            "apikey"=> "4YwLki3nduP8"
+        ];
+        $payload = json_encode($data);
+
+        $ch = curl_init("https://whatsva.com/api/sendMessageText");
+        # Setup request to send json via POST.
+
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $payload);
+
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type:application/json'));
+        # Return response instead of printing.
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    //    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        # Send request.
+        $result = curl_exec($ch);
+        curl_close($ch);
+        # Print response.
+        print_r($result);
